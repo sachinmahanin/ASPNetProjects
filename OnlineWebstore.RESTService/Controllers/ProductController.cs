8eb7b672fa9ca7cde38b0b213fd444a1833855b3
sachinmahanin <sachinmahajanin@hotmail.com>
2018-01-17 10:43:47 +0100

    partial 3

---
+++ OnlineWebstore.RESTService/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OnlineWebstore.BusinessLayer.BusinessEntities;
+using OnlineWebstore.BusinessLayer.ServiceInterface;
+using OnlineWebstore.BusinessLayer.ServiceImplementation;
+using System.Web.Http.Description;
+
+namespace OnlineWebstore.RESTService.Controllers
+{
+ 
+    public class ProductController : ApiController
+    {
+         IProductManagerService projectManager = new  ProductManagerService();
+         
+        [ResponseType(typeof(Product))]
+        [HttpGet]
+        public IHttpActionResult Get(string ProductId)
+        {
+
+            int id = 0;
+            if (string.IsNullOrEmpty(ProductId))
+                return   BadRequest("Product Id can't be empty" );
+            try
+            {
+               id=Convert.ToInt32(ProductId);
+             }
+            catch
+            {
+                return BadRequest("Product Id can't be empty");
+
+            }
+            var product =projectManager.GetProduct(id);
+            if(product==null)
+                 return NotFound();
+            return Ok(product);
+        }
+
+    }
+}
---
+++ TestApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineWebstore.BusinessLayer.ServiceImplementation;
+using OnlineWebstore.BusinessLayer.ServiceInterface;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductManagerService ProductManagerService = new ProductManagerService();
+            var result = ProductManagerService.GetAllProductCategories();
+        }
+    }
+}
