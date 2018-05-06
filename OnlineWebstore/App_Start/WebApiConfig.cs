41c08b344cb04124b04010cc1f9b7d51076fddc8
Sachin Mahajan <sachinmahajanin@hotmail.com>
2018-05-06 07:42:32 +0200

    added CORS & JSON DATA Indentation and camel casing

--- OnlineWebstore/App_Start/WebApiConfig.cs
+++ OnlineWebstore/App_Start/WebApiConfig.cs
@@ -1,5 +1,6 @@
 ﻿using CacheCow.Server;
 using Microsoft.Practices.Unity;
+using Newtonsoft.Json.Serialization;
 using OnlineWebstore.BusinessLayer.ServiceImplementation;
 using OnlineWebstore.BusinessLayer.ServiceInterface;
 using System;
@@ -26,6 +27,13 @@ namespace OnlineWebstore
             var container = new UnityContainer();
             container.RegisterType<IProductManagerService, ProductManagerService>(new HierarchicalLifetimeManager());
             config.DependencyResolver = new UnityResolver(container);
+            //Enabling CORS
+            var objEnableCorsAttribute = new System.Web.Http.Cors.EnableCorsAttribute("*","*","*");
+            config.EnableCors(objEnableCorsAttribute);
+            // if you want to indent the JSON DATA 
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //if you want to have a camel case in property names then
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             //Configure HTTP Caching using Entity Tags (ETags)
             GlobalConfiguration.Configuration.MessageHandlers.Add(new  CachingHandler(GlobalConfiguration.Configuration));
         }
