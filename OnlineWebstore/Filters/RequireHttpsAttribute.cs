99eefc39408dab00b59437009b4a054ce7ff2e39
Sachin Mahajan <sachinmahajanin@hotmail.com>
2018-05-06 08:30:49 +0200

    added RequireHttps Authorization filter

---
+++ OnlineWebstore/Filters/RequireHttpsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Net.Http;
+namespace OnlineWebstore.Filters
+{
+    public class RequireHttpsAttribute:AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Found);
+                actionContext.Response.Content = new StringContent("<p> Use HTTPS in place of HTTP");
+                UriBuilder uriBuilder = new UriBuilder(actionContext.Request.RequestUri);
+                uriBuilder.Scheme = Uri.UriSchemeHttps;
+                uriBuilder.Port = 44343;
+                actionContext.Response.Headers.Location = uriBuilder.Uri;
+            }
+            else
+            {
+                base.OnAuthorization(actionContext);
+            }
+        }
+        
+    }
+}
